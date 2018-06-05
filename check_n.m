446ae4bb7253efaf78035b81bfaa3238bdf5b87e
JY <lovedct_jy@naver.com>
2018-06-05 15:29:03 +0900

    종료조건 수정

--- check_n.m
+++ check_n.m
@@ -1,15 +1,14 @@
-G = dot(23,:);
+function n = check_n(G,dot,p,a)
 n=1;
 R = G;
 
 while 1
-    R(end,:)
     n = n+1;
     r = add_dot(G,R(end,:),p,a);
  
     
     if ismember(r,dot)
-        R = [R; r]
+        R = [R; r];
     else
         fprintf("dot(" + r(1) + "," + r(2) + ") is not on curve")
         break;
@@ -17,9 +16,8 @@ while 1
     
     if r == [0,0];
         break;
-    elseif (r(1)==G(1)) && (r(2)~=G(2));
-        break;
     elseif r(2)==0 && R(end-1,2)==0
         break;
     end
+end
 end
