f87f1bb72ea06dc59e3451c51fdb2e4e8e816722
JY <lovedct_jy@naver.com>
2018-06-06 21:28:28 +0900

    p 변경

--- public_parameter.m
+++ public_parameter.m
@@ -2,8 +2,8 @@ clc; clear;
 
 %%%% 파라미터 지정 
 %%%% p_소수 // a,b_타원곡선 정의 계수 // G_base point // n_order of G
-%%%% 발표용 p = 491 / a = 2 / b = 0 // p는 p=(4*i)+1을 만족해야 한다
-p = 491; a = 2; b = 0;
+%%%% 발표용 p = 599 / a = 2 / b = 0 // p는 p=(4*i)+1을 만족해야 한다
+p = 599; a = 2; b = 0;
 find_dot(p,a,b);
 load('parameter.mat');
 [G n] = generate_G(dot,p,a);
